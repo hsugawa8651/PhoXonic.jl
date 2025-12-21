19d9371a548082e3704cc2a8c3652f74b8cd9249
Hiroharu Sugawara <hsugawa@gmail.com>
2025-12-21 19:16:02 +0900

    Initialize TMM module structure

---
+++ src/tmm/TMM.jl
@@ -0,0 +1,8 @@
+# Transfer Matrix Method module for 1D structures
+#
+# Provides transmission/reflection spectrum calculations
+# for photonic and phononic multilayer structures.
+
+# Types will be added here
+
+# Functions will be added here
---
+++ test/tmm/runtests.jl
@@ -0,0 +1,8 @@
+# TMM module tests
+
+using Test
+using PhoXonic
+
+@testset "TMM" begin
+    # Tests will be added here
+end
