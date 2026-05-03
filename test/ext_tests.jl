5d497a4076e061434a8127bd62aa2b0ed744d3c8
Hiroharu Sugawara <hsugawa@gmail.com>
2026-05-03 13:56:17 +0900

    test: include rsk_ext in ext group (#54)

--- test/ext_tests.jl
+++ test/ext_tests.jl
@@ -1,4 +1,7 @@
 @testset "PhoXonic.jl (ext)" begin
     # PythonPlot extension: savefig_publication
     include("test_publication.jl")
+
+    # ReducedShiftedKrylov extension
+    include("rsk_ext/runtests.jl")
 end
